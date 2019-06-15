ee1fd13ceda3272ee319fac49bb69283f1844ff1
Syxooper19 <bastien.roussel.19@gmail.com>
2019-06-15 11:48:51 +0200

    api meteo

--- raspberry/lecture_humidite_temperature.py
+++ raspberry/lecture_humidite_temperature.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 #
 
+#import pour authentification google
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pi/Documents/IOTerra-API/raspberry/IOTerra-fb51c3b256a4.json"
+
 # infos mail
 import conf
 
@@ -26,17 +30,23 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
+#import pour interroger une api
+import requests
+
+
+
 def detecteProbleme(hygroMesure, tempMesure):
     paramTerra  = db.collection(u'Terrarium').document('KreKAs5CarjyWiko68bv')
     doc         = paramTerra.get()
     adresseMail = doc.to_dict().get('AlertesSouhaitee')
 
     if (adresseMail != ""):
-        hygrometrie     =   doc.to_dict().get('HygrométrieSouhaitee')
+        hygrometrie     =   doc.to_dict().get('HygrometrieSouhaitee')
         temperature     =   doc.to_dict().get('temperatureSouhaitee')
         toleranceHygro  =   doc.to_dict().get('toleranceHygro')
         toleranceTemp   =   doc.to_dict().get('toleranceTemp')
         
+
         #Si l'hygrométrie est non conforme
         if (float(hygroMesure) < float(hygrometrie)*(1+float(toleranceHygro)/100)) & (float(hygroMesure) > float(hygrometrie)*(1-float(toleranceHygro)/100)):
             print('hygrometrie conforme')
@@ -114,27 +124,51 @@ sensor = 4
 blue = 0    
 white = 1
 
+#infos Météo
+url     =   "http://api.openweathermap.org/data/2.5/weather?q=Amiens,fr&appid="
+apiKey  =   "f8ccfd42a1e358129e1d66848c319677"
+
+
 
 try:
-    [temp,humidity] = grovepi.dht(sensor,blue)  
-    sensor_value = grovepi.analogRead(light_sensor)
-
+    
+ 
+    
+    #Lecture des données capteurs
+    [temp,humidity]         = grovepi.dht(sensor,blue)  
+    sensor_value            = grovepi.analogRead(light_sensor)
+    
+    #Lecture température Extérieure
+    content                 =   requests.get(url+apiKey)
+    data                    =   content.json()
+    temperatureExterieure   =   data['main']['temp']
+    
+    for entry in data['weather']:
+        meteo = entry['main']
+   
 
    
     if math.isnan(temp) == False and math.isnan(humidity) == False and math.isnan(sensor_value) == False:
         print("temperature = %.02f C hygrométrie =%.02f%%"%(temp, humidity))
         print("luminosité = %d" %(sensor_value))
 
-
+        print("La témpérature est de {} degrés C".format(temperatureExterieure-273.15))
+        print(meteo)
+        
+        
         data = {
             u'temperature': temp,
             u'hygrometrie': humidity,
             u'luminosite':sensor_value,
+            u'temperatureExterieur':round(temperatureExterieure-273.15,1),
+            u'meteo':meteo,
             'date':now        
         }
+        temp = str(temp)
+        humidity = str(humidity)
 
         # Add a new doc in collection 'cities' with ID 'LA'
-        #db.collection(u'Terrarium').document('KreKAs5CarjyWiko68bv').collection(u'Mesures').document().set(data)
+        db.collection(u'Terrarium').document('KreKAs5CarjyWiko68bv').collection(u'Mesures').document().set(data)
         detecteProbleme(humidity, temp)
         
 except IOError:
